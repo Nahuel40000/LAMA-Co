a6f93c8281b6910b70a2708f2688af64ba119012
Basile le Boss <Bmaillard@wl2s-232-146.unil.ch>
2016-04-22 12:57:29 +0200

    Ajout du login + suppression de l'inutile
    Pas encore terminé.

--- Lama/Lama.js
+++ Lama/Lama.js
@@ -2,23 +2,18 @@ AnnonceList = new Mongo.Collection('annonce');
 UserList = new Mongo.Collection('user');
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  
+  Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
     }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
+});
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
