4955143b2f4a47e7674c8ade709cae1de200251e
Lucien-LAMA <lucien.chapuisat@unil.ch>
2016-04-26 10:45:13 +0200

    Delete inutile
    Delete le code de base js

--- Lama1.3/client/main.js
+++ Lama1.3/client/main.js
@@ -5,24 +5,6 @@ import './main.html';
 
 AnnonceList = new Mongo.Collection('annonce');
 UserList = new Mongo.Collection('user');
-			//LE CODE DE BASE DU JS CLIENT
-			//			Template.hello.onCreated(function helloOnCreated() {
-			//			  // counter starts at 0
-			//			  this.counter = new ReactiveVar(0);
-			//			});
-
-			//			Template.hello.helpers({
-			//			  counter() {
-			//			    return Template.instance().counter.get();
-			//			  },
-			//			});
-
-			//			Template.hello.events({
-			//			  'click button'(event, instance) {
-						    // increment the counter when button is clicked
-			//			    instance.counter.set(instance.counter.get() + 1);
-			//			  },
-			//			});
 						
 //Le code qui correspond au if(meteorisclient) de l'ancien fichier 1.2
   Template.register.events({
