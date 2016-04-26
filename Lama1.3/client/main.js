c0ec3aedf20f6f2c48edbed1f0993efc57891d18
Lucien-LAMA <lucien.chapuisat@unil.ch>
2016-04-26 10:07:48 +0200

    Ajout code qu'on avait déjà dans Lama1.3
    Il faut qu'on bosse sur Lama1.3, du coup j'ai déplacé le code qu'on
    avait déjà dans Lama --> Lama 1.3

--- Lama1.3/client/main.js
+++ Lama1.3/client/main.js
@@ -2,21 +2,35 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+AnnonceList = new Mongo.Collection('annonce');
+UserList = new Mongo.Collection('user');
+						//LE CODE DE BASE DU JS CLIENT
+						Template.hello.onCreated(function helloOnCreated() {
+						  // counter starts at 0
+						  this.counter = new ReactiveVar(0);
+						});
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+						Template.hello.helpers({
+						  counter() {
+						    return Template.instance().counter.get();
+						  },
+						});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+						Template.hello.events({
+						  'click button'(event, instance) {
+						    // increment the counter when button is clicked
+						    instance.counter.set(instance.counter.get() + 1);
+						  },
+						});
+						
+//Le code qui correspond au if(meteorisclient) de l'ancien fichier 1.2
+  Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+    }
