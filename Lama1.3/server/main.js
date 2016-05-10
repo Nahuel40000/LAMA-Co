8e1a004fa2ea742c5b39258716a03781c444f2c7
Lucien-LAMA <lucien.chapuisat@unil.ch>
2016-05-10 09:54:19 +0200

    Autoform Marche
    Ajout de formulaire dans la DB marche

--- Lama1.3/server/main.js
+++ Lama1.3/server/main.js
@@ -3,3 +3,33 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+AnnonceList = new Mongo.Collection('annonce');
+
+//package Collection2 pour la structure de la DB --> je vais encore le changer pour que ça corresponde à ce qu'on avait dit (c'était juste pour tester)
+AnnonceList.attachSchema(new SimpleSchema({
+  title: {
+    type: String,
+    label: "Title",
+    max: 200
+  },
+  author: {
+    type: String,
+    label: "Author"
+  },
+  copies: {
+    type: Number,
+    label: "Number of copies",
+    min: 0
+  },
+  etat: {
+    type: String,
+    label: "Etat du livre"
+  },
+  remarque: {
+    type: String,
+    label: "Remarque",
+    optional: true,
+    max: 1000
+  }
+}));
