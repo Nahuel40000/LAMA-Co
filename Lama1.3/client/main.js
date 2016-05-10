4b2d5c28ed68bdfe9c24a9e17d80fbf0d3cd2a8d
Lucien-LAMA <lucien.chapuisat@unil.ch>
2016-05-10 10:09:50 +0200

    Ajout code CLient pour autoform
    EN fait le code de tout à l'heure était utile

--- Lama1.3/client/main.js
+++ Lama1.3/client/main.js
@@ -6,7 +6,32 @@ import './main.html';
 AnnonceList = new Mongo.Collection('annonce');
 UserList = new Mongo.Collection('user');
 
-
+AnnonceList.attachSchema(new SimpleSchema({
+  title: {
+    type: String,
+    label: "Title",
+    max: 200
+  },
+  author: {
+    type: String,
+    label: "Author"
+  },
+  copies: {
+    type: Number,
+    label: "Number of copies",
+    min: 0
+  },
+  etat: {
+    type: String,
+    label: "Etat du livre"
+  },
+  remarque: {
+    type: String,
+    label: "Remarque",
+    optional: true,
+    max: 1000
+  }
+}));
 	
 
 
