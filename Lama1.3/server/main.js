dc42ad2551e61ffb3ea7519216f922e12d7b1688
Lucien-LAMA <lucien.chapuisat@unil.ch>
2016-05-10 09:16:03 +0200

    Autoform bug
    Autoform qui marchait mais qui marche plus

--- Lama1.3/server/main.js
+++ Lama1.3/server/main.js
@@ -3,3 +3,35 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+
+AnnonceList = new Mongo.Collection('annonce');
+
+//package Collection2 pour la structure de la DB --> je vais encore le changer pour que ça corresponde à ce qu'on avait dit (c'était juste pour tester)
+AnnonceList.attachSchema(new SimpleSchema({
+  title: {
+    type: String,
+    label: "Title",
+    max: 300
+  },
+  author: {
+    type: String,
+    label: "Auteur"
+  },
+  copies: {
+    type: Number,
+    label: "Prix",
+    min: 0
+  },
+  etat: {
+    type: String,
+    label: "Etat du livre"
+  },
+  remarque: {
+    type: String,
+    label: "Remarque",
+    optional: true,
+    max: 1000
+  }
+}));
