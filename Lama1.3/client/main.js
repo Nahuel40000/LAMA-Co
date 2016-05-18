020b592a2e336476eeb73e7202f94455e9f01478
Basile le Boss <Bmaillard@wl2s-236-108.unil.ch>
2016-05-18 14:35:12 +0200

    Ajout de la vérification par mail

--- Lama1.3/client/main.js
+++ Lama1.3/client/main.js
@@ -33,16 +33,60 @@ AnnonceList.attachSchema(new SimpleSchema({
 }));
 	
 
+// Toute cette partie contient du code lié au LOGIN 
+  
+Template.signup.events({
+  'submit form' ( event, template ) {
+    event.preventDefault(); // création d'une event lorsque l'utilisateur soumet sa demande de création de compte
+    
+    let user = {
+      email: template.find( '[name="emailAddress"]' ).value,
+      password: template.find( '[name="password"]' ).value
+    }; // récupération de l'email de l'utilisateur demandant la création de compte
 
-//Le code qui correspond au if(meteorisclient) de l'ancien fichier 1.2
-  Template.register.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var email = $('[name=email]').val();
-        var password = $('[name=password]').val();
-        Accounts.createUser({
-            email: email,
-            password: password
+    Accounts.createUser( user, ( error ) => {
+      if ( error ) {
+        Bert.alert( error.reason, 'danger' );
+      } else {
+        Meteor.call( 'sendVerificationLink', ( error, response ) => {
+          if ( error ) {
+            Bert.alert( error.reason, 'danger' );
+          } else {
+            Bert.alert( 'Bienvenue!', 'success' );
+          }
         });
-    }
-  });
+      }
+    });
+  }
+}); // Génère des alertes grâce à Bert en fonction des données entrées par l'utilisateur (mauvais mdp, email. etc.)
+
+
+
+Template.index.events({
+  'click .resend-verification-link' ( event, template ) {
+    Meteor.call( 'sendVerificationLink', ( error, response ) => {
+      if ( error ) {
+        Bert.alert( error.reason, 'danger' );
+      } else {
+        let email = Meteor.user().emails[ 0 ].address;
+        Bert.alert( `Vérification envoyée à ${ email }!`, 'success' );
+      }
+    });
+  }
+}); // lien pour renvoyé un mail de vérification au format alerte Bert
+
+FlowRouter.route( '/verify-email/:token', {
+  name: 'verify-email',
+  action( params ) {
+    Accounts.verifyEmail( params.token, ( error ) =>{
+      if ( error ) {
+        Bert.alert( error.reason, 'danger' );
+      } else {
+        FlowRouter.go( '/' );
+        Bert.alert( 'Vérification réussie! Merci et bonne visite!', 'success' );
+      }
+    });
+  }
+}); // Création du routing de vérification grâce à Flowrouter. 
+
+// Fin de la partie LOGIN 
--- Lama1.3/server/main.js
+++ Lama1.3/server/main.js
@@ -4,6 +4,58 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
+Meteor.startup( function() {
+  process.env.MAIL_URL = "smtp://postmaster%40sandboxa02d0e41cae7443fbe64c29545e8a256.mailgun.org:0448f718cfdda8a1ef3b3cfb0f55b165@smtp.mailgun.org:587";
+
+}); // Serveur d'envoi Mailgun
+
+Meteor.methods({
+  sendVerificationLink() {
+    let userId = Meteor.userId();
+    if ( userId ) {
+      return Accounts.sendVerificationEmail( userId );
+    }
+  }
+}); // récupération du mail utilisé en login pour l'envoi mail 
+
+  
+Accounts.emailTemplates.siteName = "UniBooks";
+Accounts.emailTemplates.from     = "UniBooks <admin@unibooks.com>";
+
+Accounts.emailTemplates.verifyEmail = {
+  subject() {
+    return "[UniBooks] Verify Your Email Address";
+  },
+  text( user, url ) {
+    let emailAddress   = user.emails[0].address,
+        urlWithoutHash = url.replace( '#/', '' ),
+        supportEmail   = "support@unibooks.com",
+        emailBody      = `To verify your email address (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+
+    return emailBody;
+  }
+}; // Paramétrage de l'email envoyé (Adresse d'envoi, sujet, objet, contenu texte + URL de vérification)
+
+FlowRouter.route( '/verify-email/:token', {
+  name: 'verify-email',
+  action( params ) {
+    Accounts.verifyEmail( params.token, ( error ) =>{
+      if ( error ) {
+        Bert.alert( error.reason, 'danger' );
+      } else {
+        FlowRouter.go( '/' );
+        Bert.alert( 'Email verified! Thanks!', 'success' );
+      }
+    });
+  }
+}); // création d'une route Flowrouter pour la vérification du lien (vérification booléenne du login)
+
+
+
+Accounts.config({restrictCreationByEmailDomain:'unil.ch'}); // restriction du domaine mail
+
+
+
 AnnonceList = new Mongo.Collection('annonce');
 
 //package Collection2 pour la structure de la DB --> je vais encore le changer pour que ça corresponde à ce qu'on avait dit (c'était juste pour tester)
