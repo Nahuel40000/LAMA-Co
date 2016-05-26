d872b1cf7bd3ff4782040d6828aad75f7b4c0fe8
Basile le Boss <Bmaillard@wl2s-163-188.unil.ch>
2016-05-26 09:25:33 +0200

    suppression de problèmes txt

--- Lama1.3/server/main.js
+++ Lama1.3/server/main.js
@@ -4,7 +4,6 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
-<<<<<<< HEAD
 Meteor.startup( function() {
   process.env.MAIL_URL = "smtp://postmaster%40sandboxa02d0e41cae7443fbe64c29545e8a256.mailgun.org:0448f718cfdda8a1ef3b3cfb0f55b165@smtp.mailgun.org:587";
 
@@ -57,10 +56,6 @@ FlowRouter.route( '/verify-email/:token', {
 
 
 Accounts.config({restrictCreationByEmailDomain:'unil.ch'}); // restriction du domaine mail
-
-=======
-Accounts.config({restrictCreationByEmailDomain:'unil.ch', sendVerificationEmail: true}); // restriction du domaine mail
->>>>>>> master
 
 
 AnnonceList = new Mongo.Collection('annonce');
